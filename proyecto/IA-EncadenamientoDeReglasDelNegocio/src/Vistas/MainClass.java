2ce373cce3a6446285d5d9c5422cb0b816bcc151
Angelo Licetti León <angelo.licetti.00@gmail.com>
2020-07-28 14:30:16 -0500

    proyecto final agregado

--- proyecto/IA-EncadenamientoDeReglasDelNegocio/src/Vistas/MainClass.java
+++
@@ -1,8 +0,0 @@
-package Vistas;
-
-public class MainClass {
-    public static SimuladorDeCreditoHipotecario pequeñoSimulador = new SimuladorDeCreditoHipotecario();
-    public static void main(String args[]){
-        pequeñoSimulador.setVisible(true);
-    }
-}
